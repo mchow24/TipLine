a8b1716cc5bab7cd82f26ac53a91fb2c1dd37a61
Nikhil Sharma <nikhil34201@gmail.com>
2023-06-08 14:02:40 -0700

    Added Frontend

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -3,7 +3,16 @@ import { Bucket, Table } from "sst/constructs";
 export function StorageStack({ stack, app }) {
     // Create the DynamoDB table
     // Create an S3 bucket
-    const bucket = new Bucket(stack, "Uploads");
+    const bucket = new Bucket(stack, "Uploads", {
+        cors: [
+          {
+            maxAge: "1 day",
+            allowedOrigins: ["*"],
+            allowedHeaders: ["*"],
+            allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+          },
+        ],
+    });
 
     const table = new Table(stack, "Posts", {
         fields: {
